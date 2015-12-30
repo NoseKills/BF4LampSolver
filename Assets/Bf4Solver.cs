b16fe4f99c7b15412ca144a31d8e8a3d3216238e
Sampo <srantaaho@gmail.com>
2015-12-30 23:53:09 +0200

    test data

--- Assets/Bf4Solver.cs
+++ Assets/Bf4Solver.cs
@@ -36,8 +36,33 @@ public class Bf4Solver : MonoBehaviour {
 			var l = (Location) i;
 			Map.Add(l, PlayerPrefs.GetInt(l.ToString(), 0));
 		}
+		//SetTestData();
 		LocationChanged(true);
 		initialized = true;
+	}
+
+	private void SetTestData() {
+		/*Map[Location.StartingSituation] = 1 << 10 | 1 << 16;
+		Map[Location.Garden] = 1 << 2 | 1 << 3 | 1 << 7 | 1 << 10 | 1 << 14 | 1 << 16 | 1 << 18;
+		Map[Location.Tree] = 1 << 3 | 1 << 7 | 1 << 8 | 1 << 10 | 1 << 11 | 1 << 13 | 1 << 14 | 1 << 16;
+		Map[Location.Pagoda] = 1 << 0 | 1 << 1 | 1 << 2 | 1 << 9 | 1 << 10 | 1 << 15 | 1 << 16 | 1 << 18;
+		Map[Location.Furnace] = 1 << 0 | 1 << 5 | 1 << 6 | 1 << 8 | 1 << 9 | 1 << 10 | 1 << 11 | 1 << 13 | 1<<16 | 1<<19;
+		Map[Location.Pier] = 1 << 8 | 1 << 11 | 1 << 13;
+		Map[Location.Waterfall] = 1 << 4 | 1 << 12 | 1 << 17;
+		Map[Location.Rock] = 1 << 4 | 1 << 5 | 1 << 6 | 1 << 10 | 1 << 12 | 1 << 16 | 1 << 17 | 1 << 19;*/
+
+		// 2nd test case from commenter on reddit.  Not sure of data quality
+		
+		Map[Location.StartingSituation] = 1 << 0 | 1 << 8 | 1 << 13;
+		Map[Location.Garden] = 1 << 1 | 1 << 4 | 1 << 5 | 1 << 7 | 1 << 15 | 1 << 19;
+		Map[Location.Tree] = 1 << 1 | 1 << 2 | 1 << 3 | 1 << 5 | 1 << 9 | 1 << 19;
+		Map[Location.Pagoda] = 1 << 1 | 1 << 5 | 1 << 16 | 1 << 17 | 1 << 18 | 1 << 19;
+		Map[Location.Furnace] = 1 << 0 | 1 << 2 | 1 << 3 | 1 << 8 | 1 << 11 | 1 << 13 | 1 << 14;
+		Map[Location.Pier] = 1 << 4 | 1 << 6 | 1 << 7 | 1 << 10 | 1 << 12 | 1 << 15;
+		Map[Location.Waterfall] = 1 << 6 | 1 << 9 | 1 << 10 | 1 << 12;
+		Map[Location.Rock] = 1 << 0 | 1 << 8 | 1 << 13 | 1 << 16 | 1 << 17 | 1 << 18;
+
+		Map[Location.Currently] = Map[Location.StartingSituation];
 	}
 
 	public void LocationChanged(bool b) {
